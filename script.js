33b18ef4e9eeb66d58902b3de5216749c2de7d37
dechamma43-sys <dechamma43@gmail.com>
2026-04-28 18:49:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let chart;
 function showData() {
     const ctx = document.getElementById('myChart');
 
+    // Destroy old chart if exists (prevents overlap)
     if (chart) {
         chart.destroy();
     }
@@ -13,8 +14,31 @@ function showData() {
             labels: ['Social Media', 'Emails', 'Comments'],
             datasets: [{
                 label: 'Digital Footprint',
-                data: [70, 50, 30]
+                data: [70, 50, 30],
+                borderWidth: 1
             }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: 'white'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white'
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: 'white'
+                    }
+                }
+            }
         }
     });
 }
